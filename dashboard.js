34c30c9bd8d6198cec679e08ab3758f09d51092a
trideep7 <83818644+trideep7@users.noreply.github.com>
2021-05-07 19:49:14 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -12,23 +12,23 @@
     type: 'line',
     data: {
       labels: [
-		'30th April',
-        '1st May',
-        '2nd May',
-        '3rd May',
-        '4th May',
-        '5th May',
-        '6th May'
+        	'1-May',
+        	'2-May',
+        	'3-May',
+        	'4-May',
+        	'5-May',
+        	'6-May',
+	      	'7-May'
       ],
       datasets: [{
         data: [
-          229,
-          262,
-          321,
-          246,
-          339,
-          269,
-          347
+          	262,
+          	321,
+          	246,
+          	339,
+          	269,
+          	347,
+		297
         ],
         lineTension: 0,
         backgroundColor: 'transparent',
