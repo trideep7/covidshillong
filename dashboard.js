e57e3d3484efaf8bc0412d9682691735b73766e4
trideep7 <83818644+trideep7@users.noreply.github.com>
2021-05-18 15:51:42 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -12,24 +12,24 @@
     type: 'line',
     data: {
       labels: [
-	      '11-May',
               '12-May',
 	      '13-May',
 	      '14-May',
 	      '15-May',
 	      '16-May',
-	      '17-May'
+	      '17-May',
+	      '18-May'
       ],
       datasets: [{
 	label: 'Per Day Cases',
         data: [
-		450,
                 406,
 		591,
 		627,
 		560,
 		569,
-		634
+		634,
+		906
         ], 
         lineTension: 0,
         backgroundColor: 'transparent',
@@ -40,13 +40,13 @@
       {
         label: 'Per Day Deaths',
         data: [
-	    	9,
             	8,
 		18,
 		12,
 		21,
 		19,
-		16
+		16,
+		19
         ],
         lineTension: 0,
         backgroundColor: 'transparent',
@@ -57,13 +57,13 @@
 	{
         label: 'Per Day Recoveries',
         data: [
-	    	206,
             	314,
 		228,
 		327,
 		215,
 		354,
-		237
+		237,
+		470
         ],
         lineTension: 0,
         backgroundColor: 'transparent',
